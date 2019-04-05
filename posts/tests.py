0d54ecbe7373470287708ad2cc9ccaab49aa6eae
Hyacinthe04 <mnuriarth28@gmail.com>
2019-04-05 16:45:50 +0200

    add readme file

--- posts/tests.py
+++ posts/tests.py
@@ -21,14 +21,14 @@ class ProjectTestClass(TestCase):
     '''
 
     def setUp(self):
-        self.proj= Project(title = 'Football', description ="The Lorem Ipsum is simply dummy text of the composition and layout before printing. Lorem Ipsum has been the standard text for printing since the 1500s, when an anonymous printer assembled pieces of text together to make a sample book of text fonts. It has not only survived five centuries, but has also adapted to computer office, without its content is changed.",link:'more')
+        self.proj= Project(title = 'Football', description ='The Lorem Ipsum is simply dummy text of the composition and layout before printing. Lorem Ipsum has been the standard text for printing since the 1500s, when an anonymous printer assembled pieces of text together to make a sample book of text fonts. It has not only survived five centuries, but has also adapted to computer office, without its content is changed',link='more')
         self.proj.save()  
 
     def test_instance(self):
         self.assertTrue(isinstance(self.title,Project))
 
     def test_save_method(self):
-        self.title.save()
+        self.proj.save()
         title = Project.objects.all()
         self.assertTrue(len(title) > 0)
 
